72223ca8ae6714b9aa7575e95c31258f284de7f3
juhipark <juhipark@student.hartnell.edu>
2018-10-01 23:08:42 -0700

    First commit

---
+++ app/src/main/java/comjuhiparkcodepath_prework/httpsgithub/pre_work/MainActivity.java
@@ -0,0 +1,22 @@
+package comjuhiparkcodepath_prework.httpsgithub.pre_work;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView)findViewById(R.id.textView)).setTextColor(getResources().getColor(R.color.colorPrimary));
+            }
+        });
+    }
+}
